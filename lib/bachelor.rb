bf74bd79d6c50dcc8edab9338b1bfce4e24adfb9
Henry Sztanski <henry@sztanski.com>
2018-12-15 21:47:29 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,7 +5,7 @@ def get_first_name_of_season_winner(data, season)
   # code here
   season_k = season.to_sym
   binding.pry
-  data[season_k].each do |element|
+  data[season].each do |element|
     if element[:status] == "Winner"
       return element[:name].split.first
     end
